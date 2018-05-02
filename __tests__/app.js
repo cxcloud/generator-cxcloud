96abb27d9de9000661ab4a5ebe8abc14f65996a0
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-05-02 12:53:11 +0300

    Fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -11,6 +11,6 @@ describe('generator-cxcloud:app', () => {
   });
 
   it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+    assert.file(['package.json']);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,59 +1,60 @@
-"use strict";
-const Generator = require("yeoman-generator");
-const chalk = require("chalk");
-const yosay = require("yosay");
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
 
 module.exports = class extends Generator {
   prompting() {
     this.log(
+      // eslint-disable-next-line prettier/prettier
       yosay(
-        `Welcome to the impeccable ${chalk.red("generator-cxcloud")} generator!`
+        `Welcome to the impeccable ${chalk.red('generator-cxcloud')} generator!`
       )
     );
 
     const prompts = [
       {
-        type: "input",
-        name: "projectName",
-        message: "Enter a DNS compatible project name",
+        type: 'input',
+        name: 'projectName',
+        message: 'Enter a DNS compatible project name',
         default: this.appname
       },
       {
-        type: "input",
-        name: "projectDescription",
-        message: "Enter the project description"
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Enter the project description',
+        default: 'A CXCloud Microservice'
       },
       {
-        type: "input",
-        name: "orgName",
-        message: "Enter your Github username/organization name",
+        type: 'input',
+        name: 'orgName',
+        message: 'Enter your Github username/organization name',
         default: this.user.github.username
       },
       {
-        type: "input",
-        name: "authorInfo",
-        message: "Enter your name and email",
+        type: 'input',
+        name: 'authorInfo',
+        message: 'Enter your name and email',
         default: `${this.user.git.name()} <${this.user.git.email()}>`
       }
     ];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath("package.json"),
-      this.destinationPath("package.json"),
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
       this.props
     );
   }
 
   install() {
-    // this.installDependencies({
-    //   bower: false
-    // });
+    this.installDependencies({
+      bower: false
+    });
   }
 };
