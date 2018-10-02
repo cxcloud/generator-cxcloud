e2dd363c1f60ea5b6ef7004f4ae13ed048e440a1
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-10-02 15:36:03 +0300

    Fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,9 +5,19 @@ const helpers = require('yeoman-test');
 
 describe('generator-cxcloud:app', () => {
   beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ isDeployedToKube: false });
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      isDeployedToKube: false,
+      projectName: 'test',
+      projectDescription: 'test',
+      orgName: 'test',
+      authorName: 'test',
+      authorEmail: 'test@test.com',
+      services: [],
+      ecrRepository: '',
+      isRouted: false,
+      kubeDomain: '',
+      isSecure: false
+    });
   });
 
   it('creates files', () => {
