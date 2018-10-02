a371b8a0024bd4453eb6d6f2d3c6cd6fcdf50e19
linda <linda.damario@tieto.com>
2018-10-02 12:41:13 +0300

    Fix indentation of query.ts

--- generators/app/templates/service/src/utils/query.ts
+++ generators/app/templates/service/src/utils/query.ts
@@ -1,15 +1,15 @@
-import { QueryOptions } from '@cxcloud/ct-types/common';
+import { QueryOptions } from '@cxcloud/ct-types/common'; 
 
 export function getQueryOptions(
-	page?: number,
-	perPage?: number,
-	sortPath?: string,
-	ascending?: boolean
+  page?: number,
+  perPage?: number,
+  sortPath?: string,
+  ascending?: boolean
 ): QueryOptions {
-	return {
-		page: typeof page !== 'number' || page < 1 ? 1 : page,
-		perPage: typeof perPage !== 'number' || perPage < 1 ? 20 : perPage,
-		sortPath,
-		ascending
-	};
+  return {
+	page: typeof page !== 'number' || page < 1 ? 1 : page,
+	perPage: typeof perPage !== 'number' || perPage < 1 ? 20 : perPage,
+	sortPath,
+	ascending
+  };
 }
